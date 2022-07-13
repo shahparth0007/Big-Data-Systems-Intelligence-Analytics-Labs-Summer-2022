a60ca941aebe8c6f62cb3a0f020595afe0fa8158
Parth Shah <shah.parth3@northeastern.edu>
2022-07-13 19:38:18 -0400

    added

---
+++ Lab5/Model/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import uvicorn
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+app = FastAPI()
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/test")
+async def root():
+    savedModel=load_model('gfgModel.h5')
+    predictions = savedModel.predict(test_images)
+    return str(predictions[1])
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
---
+++ Lab5/Model/build.py
@@ -0,0 +1,63 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10),
+    tf.keras.layers.Softmax()
+])
+
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+model.save('gfgModel.h5')
+print('Model Saved!')
+ 
+# load model
+savedModel=load_model('gfgModel.h5')
+savedModel.summary()
+
+predictions = savedModel.predict(test_images)
+print(predictions[0])
